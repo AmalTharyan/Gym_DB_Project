83662cf1a21744539a0be7d69a0f27bf44df99b8
AmalTharyan <83700867+AmalTharyan@users.noreply.github.com>
2021-12-16 22:10:19 -0500

    delete

--- GYM_Database 2/src/main/java/com/example/springtemplate/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springtemplate.repositories;
-
-import com.example.springtemplate.models.Course;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository
-        extends CrudRepository<Course, Integer> {
-}
